5f1ee4b1d2cb993356c4e71c41e3185ace1123ac
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-02 20:36:03 +0530

    feat: database connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
-  console.log("server is running on port 5001");
+  console.log("server is running on PORT:" + PORT);
 });
