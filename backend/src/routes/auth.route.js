2a4b38c9aa37469f24bb02cbccd30bf8c0cc4f73
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-04 15:18:57 +0530

    feat: create update profile picture

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
