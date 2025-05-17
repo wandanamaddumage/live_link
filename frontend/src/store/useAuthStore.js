031d36a9ef41f1f2d1db88dcae63140c52ee333d
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-17 18:53:33 +0530

    update: base URL

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,10 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
